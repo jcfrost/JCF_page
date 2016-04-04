6bf55467a0a6b8938da7b92e8500732f444ca920
jcfrost <jcfrosty64@gmail.com>
2016-04-04 15:56:50 -0400

    changes saved before 4/4/16 re-publish
    latest

--- JCF_page/obj/Release/Package/PackageTmp/js/JS_Exercise4.js
+++ JCF_page/obj/Release/Package/PackageTmp/js/JS_Exercise4.js
@@ -1,6 +1,5 @@
 // JavaScript source code
 $(function() { //Increasing/decreasing for-loop with two arrays
-	
 	$('#chkBtn4').click(function () {
 	    var orig4 = '';
 	    var rev = [];
@@ -9,8 +8,7 @@ $(function() { //Increasing/decreasing for-loop with two arrays
 		for (var i = orig4.length - 1, j = 0; i >= 0; i--, j++)
 			rev[j] = origCaps[i];
 			rev = rev.join('');
-
-		if(origCaps === rev) {
+        if(origCaps === rev) {
 			$('#result4').text(origCaps + " is a Palindrome");
 		} else {
 			$('#result4').text(origCaps + " is NOT a Palindrome");
